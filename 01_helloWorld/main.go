644e7aeb2d31b361d9648eb1e11cd5d7993c2206
sasquad92 <krzysztof.sommerrey@gmail.com>
2016-03-06 07:58:57 +0100

    Format printing

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	// https://golang.org/pkg/fmt/
+	fmt.Println(42)
+	fmt.Printf("%d - decimal \n%b - binary\n%x - hex \n%o - oct\n", 33, 33, 33, 33)
+	fmt.Printf("%#x - %#X \n", 11, 11)
+
+	fmt.Println("UTF=8")
+
+	for i := 60; i < 200; i++ {
+		fmt.Printf("%d \t %q \n", i, i)
+	}
+
 }
