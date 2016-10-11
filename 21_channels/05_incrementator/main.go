0bb98a3069b60714036f4a9e82f364f2ea521651
sasquad92 <krzysztof.sommerrey@gmail.com>
2016-10-11 19:58:03 +0200

    channels - incrementator

---
+++ 21_channels/05_incrementator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c1 := incrementator("Foo:")
+	c2 := incrementator("Bar:")
+	c3 := puller(c1)
+	c4 := puller(c2)
+	fmt.Println("Final Counter:", <-c3+<-c4) // waiting to receive
+}
+
+func incrementator(s string) chan int {
+	out := make(chan int)
+
+	go func() {
+		for i := 0; i < 20; i++ {
+			out <- 1
+			fmt.Println(s, i)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func puller(c chan int) chan int {
+	out := make(chan int)
+
+	go func() {
+		var sum int
+		for n := range c {
+			sum += n
+		}
+		out <- sum
+		close(out)
+	}()
+	return out
+}
