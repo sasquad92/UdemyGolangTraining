66f514029b66f0786e4e27f0d0cd42dc26f5778d
sasquad92 <krzysztof.sommerrey@gmail.com>
2016-10-11 19:58:17 +0200

    factorial challange

---
+++ 22_factorial/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num := factorial(10)
+
+	for n := range num {
+		fmt.Println(n)
+	}
+}
+
+func factorial(x int) chan int {
+	sum := 1
+	out := make(chan int)
+	go func() {
+		for i := 1; i <= x; i++ {
+			sum *= i
+		}
+		out <- sum
+		close(out)
+	}()
+	return out
+}
